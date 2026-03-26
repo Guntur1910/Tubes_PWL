d2ce3d2af45671cc633bbc0950c38b43e63eb883
Guntur1910 <2372062@maranatha.ac.id>
2026-03-26 10:31:31 +0700

    dashboard admin 2/2

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::middleware('auth')->group(function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+use App\Http\Controllers\Admin\DashboardController;
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
